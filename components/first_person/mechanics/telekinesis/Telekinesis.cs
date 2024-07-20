5c7697110b59e2aeb29c787d304064cf4b0e4996
ninetailsrabbit <danieldigitalspace@proton.me>
2024-07-20 12:46:24 +0100

    throwable area detector and telekinesis logic to pull and throw bodies

---
+++ components/first_person/mechanics/telekinesis/Telekinesis.cs
@@ -0,0 +1,151 @@
+using Extensionator;
+using Godot;
+using Godot.Collections;
+using GodotExtensionator;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GodotExtensionatorStarter {
+    [GlobalClass, Icon("res://components/first_person/telekinesis/telekinesis.svg")]
+    public partial class Telekinesis : Node3D {
+        #region Signals
+        [Signal]
+        public delegate void PulledThrowableEventHandler(Throwable throwable);
+
+        [Signal]
+        public delegate void ThrowedThrowableEventHandler(Throwable throwable);
+        #endregion
+
+        #region Exported variables
+        [Export] public ThrowableRayCastInteractor ThrowableRayCastInteractor { get; set; } = default!;
+        [Export] public ThrowableAreaDetector ThrowableAreaDetector { get; set; } = default!;
+        [Export(PropertyHint.Range, "0.1, 1000, 0.01")] public float ThrowableInteractorDistance = 5f;
+
+        [ExportGroup("Actions")]
+        [Export] public string PullInputAction = "pull";
+        [Export] public string ThrowInputAction = "throw";
+        [Export] public string PullAreaInputAction = "pull_area";
+        [Export] public string PushWaveInputAction = "push_wave";
+        [Export] public bool PullAbility = true;
+        [Export] public bool PullAreaAbility = true;
+        [Export] public bool PushWaveAbility = true;
+
+        [ExportGroup("Forces")]
+        [Export] public float PullPower = 15f;
+        [Export] public float ThrowPower = 10f;
+        [Export] public float MaximumMassThatCanLift = 1.5f;
+        [Export] public float AngularPower = 1.5f;
+        #endregion
+
+
+        public Array<ActiveThrowable> ActiveThrowables = [];
+        public Array<Marker3D> Slots = [];
+
+        public override void _EnterTree() {
+            ThrowableRayCastInteractor ??= this.FirstNodeOfClass<ThrowableRayCastInteractor>();
+            ThrowableRayCastInteractor ??= GetParent<ThrowableRayCastInteractor>();
+            ThrowableRayCastInteractor.TargetPosition = Vector3.Forward * ThrowableInteractorDistance;
+
+            ThrowableAreaDetector ??= this.FirstNodeOfClass<ThrowableAreaDetector>();
+            ThrowableAreaDetector ??= GetParent<ThrowableAreaDetector>();
+
+            UpdateSlots();
+        }
+
+        public override void _PhysicsProcess(double delta) {
+            // TODO - MAYBE RETHINK THIS PART TO MOVE FREELY THE OBJECT WHEN PULLED INSTEAD OF BEING CONSTANTLY PULLED TO THE SLOT
+            foreach (var activeThrowable in ActiveThrowables)
+                ApplyPullForce(activeThrowable.Body, delta);
+
+        }
+
+
+        public void Throw(Throwable body) {
+            ActiveThrowables.Remove(ActiveThrowables.FirstOrDefault(activeThrowable => activeThrowable.Body.Equals(body)));
+
+            Vector3 impulse = GetViewport().GetCamera3D().ForwardDirection() * ThrowPower;
+
+            body.Throw(impulse);
+            body.AngularVelocity = Vector3.One * AngularPower;
+
+            ThrowableRayCastInteractor.Enabled = ActiveThrowables.Count < Slots.Count;
+
+            EmitSignal(SignalName.ThrowedThrowable, body);
+        }
+
+        public void Pull(Throwable body) {
+            bool slotsAvailable = ThereAreAvailableSlots();
+
+            SetPhysicsProcess(slotsAvailable);
+            ThrowableRayCastInteractor.Enabled = slotsAvailable;
+            ThrowableAreaDetector.Monitoring = slotsAvailable;
+
+            if (slotsAvailable) {
+                if (!body.BodyLocked) {
+                    if (GetRandomFreeSlot() is Marker3D freeSlot) {
+                        body.Pull(freeSlot);
+                        ActiveThrowables.Add(new(body, freeSlot));
+                        EmitSignal(SignalName.PulledThrowable, body);
+                    }
+                }
+            }
+        }
+
+        public void ApplyPullForce(Throwable body, double? delta = null) {
+            delta ??= GetPhysicsProcessDeltaTime();
+
+            if (body.Grabber is not null) {
+                if (body.GrabModeIsDynamic())
+                    body.UpdateLinearVelocity((body.Grabber.GlobalPosition - body.GlobalPosition) * PullPower);
+                else
+                    body.GlobalPosition = body.GlobalPosition.MoveToward(body.Grabber.GlobalPosition, (float)delta * PullPower);
+            }
+        }
+
+        public List<Throwable> GetNearThrowables(bool sortedByDistance = true) {
+            if (ThrowableAreaDetector is not null) {
+                var bodies = ThrowableAreaDetector.GetOverlappingBodies().Where(body => body is Throwable).Cast<Throwable>().ToList();
+
+                if (sortedByDistance)
+                    bodies.Sort(delegate (Throwable throwableOne, Throwable throwableTwo) { return throwableOne.GlobalDistanceTo(this).CompareTo(throwableTwo.GlobalDistanceTo(this)); });
+
+                return bodies;
+            }
+
+            return [];
+        }
+
+        public bool BodyCanBeLifted(Throwable body) => body.Mass <= MaximumMassThatCanLift;
+
+        #region Slot related
+        private Array<Marker3D> AvailableSlots() {
+            var busySlots = BusySlots();
+
+            return (Array<Marker3D>)Slots.Where(slot => !busySlots.Contains(slot));
+        }
+
+        private Array<Marker3D> BusySlots() => (Array<Marker3D>)ActiveThrowables.Select(activeThrowable => activeThrowable.Slot);
+        private Marker3D? GetRandomFreeSlot() {
+            if (ThereAreAvailableSlots())
+                return AvailableSlots().RandomElement();
+
+            return null;
+        }
+
+        private bool ThereAreAvailableSlots() => ActiveThrowables.Count < Slots.Count;
+
+        private void UpdateSlots() {
+            Slots = this.GetAllChildren<Marker3D>();
+        }
+        #endregion
+
+
+
+    }
+
+    public partial class ActiveThrowable(Throwable body, Marker3D slot) : RefCounted {
+        public Throwable Body { get; set; } = body;
+        public Marker3D Slot { get; set; } = slot;
+
+    }
+}
---
+++ components/throwables/ThrowableAreaDetector.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+using GodotExtensionator;
+
+namespace GodotExtensionatorStarter {
+    public partial class ThrowableAreaDetector : Area3D {
+        public GameGlobals GameGlobals { get; set; } = null!;
+
+        public override void _EnterTree() {
+            GameGlobals = this.GetAutoloadNode<GameGlobals>();
+
+            CollisionLayer = 0;
+            CollisionMask = GameGlobals.ThrowablesCollisionLayer;
+            Monitorable = false;
+            Monitoring = true;
+            Priority = 2;
+            
+        }
+    }
+}
