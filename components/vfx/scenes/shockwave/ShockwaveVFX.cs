e88a6898dfc10dc339148ca5b8d794183a11a42b
ninetailsrabbit <danieldigitalspace@proton.me>
2024-07-23 11:21:00 +0100

    tor curve static class and shockwave effect component

---
+++ components/vfx/scenes/shockwave/ShockwaveVFX.cs
@@ -0,0 +1,59 @@
+﻿using Godot;
+
+namespace GodotExtensionatorStarter {
+
+    [GlobalClass, Icon("res://components/vfx/scenes/shockwave/shockwave.svg")]
+    public partial class ShockwaveVFX : Node2D {
+        #region Exported parameters
+        [Export] public bool Autostart = true;
+        [Export] public Color ShockwaveColor = Colors.White;
+        [Export] public Color OutlineColor = Colors.Black;
+        [Export] public bool DrawOutline = false;
+        [Export] public float StartRadius = 10f;
+        [Export] public float EndRadius = 100f;
+        [Export] public float StartWidth = 6f;
+        [Export] public float EndWidth = 0.2f;
+        [Export] public int ArcPoints = 24;
+        [Export] public float ExpandTime = 1f;
+        #endregion
+
+        public float TimeScale = 1f;
+        public float CurrentTimer = 0f;
+        public float Size = 1f;
+        public float SizeT = 1f; // Intermediate value used in the code to control the animation of the shockwave's size 
+
+        public override void _Ready() {
+            Size = StartRadius;
+            SetProcess(Autostart);
+        }
+
+        public override void _Process(double delta) {
+
+            delta *= TimeScale;
+            CurrentTimer += 1f / ExpandTime * (float)delta;
+
+            if (CurrentTimer >= 1f)
+                QueueFree();
+
+            SizeT = (float)TorCurve.Run(CurrentTimer, 1.5f, 0f, 1f);
+            Size = Mathf.Lerp(StartRadius, EndRadius, SizeT);
+
+            QueueRedraw();
+        }
+
+        public void Spawn() {
+            SetProcess(true);
+        }
+
+        public override void _Draw() {
+            var smoothnessFactor = Mathf.Lerp(StartWidth, EndWidth, SizeT);
+
+            if (DrawOutline)
+                DrawArc(Vector2.Zero, Size, 0, Mathf.Tau, ArcPoints, OutlineColor * new Color(0, 0, 0, 1f - Mathf.Pow(SizeT, 4f)), Mathf.Min(smoothnessFactor + 8f, smoothnessFactor * 4f), false);
+
+            DrawArc(Vector2.Zero, Size, 0, Mathf.Tau, ArcPoints, ShockwaveColor * new Color(1f, 1f, 1f, 1f - Mathf.Pow(SizeT, 4f)), smoothnessFactor, false);
+        }
+
+    }
+
+}
---
+++ utilities/Geometry/TorCurve.cs
@@ -0,0 +1,42 @@
+﻿using Extensionator;
+using System;
+
+namespace GodotExtensionatorStarter {
+    public static class TorCurve {
+        public static double Pinch(double value) => Pinch(value);
+        public static float Pinch(float value) => (float)Math.Pow(value, 2) * (value < 0.5f ? -1 : 1);
+        public static double Run(float x, float a = 0, float b = 0.5f, float c = 0) {
+            c = Pinch(c);
+            x = Math.Max(0, Math.Min(1, x));
+
+            var s = Math.Exp(a);
+            var s2 = 1f / (s + MathExtension.COMMON_EPSILON);
+            var t = Math.Max(0, Math.Min(1, b));
+            var u = c;
+
+            var result = 0d;
+            var c1 = 0d;
+            var c2 = 0d;
+            var c3 = 0d;
+
+            if (x < t) {
+                c1 = (t * x) / (x + s * (t - x) + MathExtension.COMMON_EPSILON);
+                c2 = t - Math.Pow(1 / (t + MathExtension.COMMON_EPSILON), s2 - 1) * Math.Pow(Math.Abs(x - t), s2);
+                c3 = Math.Pow(1 / (t + MathExtension.COMMON_EPSILON), s - 1) * Math.Pow(x, s);
+            }
+            else {
+                c1 = (1 - t) * (x - 1) / (1 - x - s * (t - x) + MathExtension.COMMON_EPSILON) + 1;
+                c2 = Math.Pow(1 / ((1 - t) + MathExtension.COMMON_EPSILON), s2 - 1) * Math.Pow(Math.Abs(x - t), s2) + t;
+                c3 = 1 - Math.Pow(1 / ((1 - t) + MathExtension.COMMON_EPSILON), s - 1) * Math.Pow(1 - x, s);
+            }
+
+            if (u <= 0)
+                result = (-u) * c2 + (1 + u) * c1;
+            else
+                result = (-u) * c3 + (1 - u) * c1;
+
+
+            return result;
+        }
+    }
+}
