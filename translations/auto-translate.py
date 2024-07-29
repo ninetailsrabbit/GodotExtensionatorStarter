64e343152ea11741978f1715d105f9e062452233
ninetailsrabbit <danieldigitalspace@proton.me>
2024-07-29 16:19:51 +0100

    a better script to dinamically create the translations from a .pot file

--- translations/auto-translate.py
+++ translations/auto-translate.py
@@ -1,8 +1,8 @@
-import os, re
+import os, re, subprocess
 
 
 def create_template_pot_file(translation_keys: dict):
-     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'translations_template.pot'), 'w+', encoding="utf8") as file:
+     with open(os.path.join(CURRENT_DIR, translation_template_name), 'w+', encoding="utf8") as file:
         file.write('''# GodotExtensionatorStarter - Translation template:
 #
 # FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
@@ -27,9 +27,19 @@ def find_translation_keys(dirpath: str):
             for file in list(filter(included_files_filter, files)):
                 filepath = os.path.join(root, file)
                 scan_file_for_translation_keys(filepath)
-        
-        create_template_pot_file(translation_keys)
-                
+
+       
+def create_or_update_translation_files(selected_locales = []):
+    translation_template_path = os.path.join(CURRENT_DIR, translation_template_name).lstrip().replace("\\", "/")
+
+    for locale in selected_locales:
+        current_po_translation_path = os.path.join(CURRENT_DIR, f"{locale}.po")
+
+        if os.path.exists(current_po_translation_path):
+            subprocess.run(['msgmerge', '--update', '--backup=none', current_po_translation_path, translation_template_path], check=True, capture_output=True, text=True)
+        else:
+            subprocess.run(['msginit', '--no-translator', f'--input={translation_template_path}', f'--locale={current_po_translation_path}'] , check=True, capture_output=True, text=True)
+
 
 def included_files_filter(file: str) -> bool:
     included_extensions = [".tscn", ".tres", ".res", ".gd", ".cs"]
@@ -47,7 +57,7 @@ def scan_file_for_translation_keys(filepath: str):
     with open(filepath, 'r', encoding="utf8") as file:
         for line in file:
             for translation_key in translation_keys_from(line):
-                translation_keys[translation_key] = os.path.relpath(filepath, ROOT_DIR).lstrip().replace("\\", "/")
+                translation_keys[translation_key] = os.path.relpath(filepath, GODOT_ROOT_DIR).lstrip().replace("\\", "/")
 
 
 def find_godot_root_dir() -> str:
@@ -67,13 +77,17 @@ def find_godot_root_dir() -> str:
     return root_dir
 
 def translation_keys_from(value: str):
-    return re.findall(r'\b[A-Z]+(?:_[A-Z]+)+\b', value)
+    return re.findall(r'\b[A-Z0-9]+(?:_[A-Z0-9]+)+\b', value)
 
 
-
+locales = ['en', 'es', 'fr', 'de', 'it', 'pt']
+translation_template_name = 'translations_template.pot'
 files_readed = []
 translation_keys = {}
 
-ROOT_DIR = find_godot_root_dir()
-
-find_translation_keys(ROOT_DIR);
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+GODOT_ROOT_DIR = find_godot_root_dir()
+                              
+find_translation_keys(GODOT_ROOT_DIR);
+create_template_pot_file(translation_keys)
+create_or_update_translation_files(locales)
