e912949132d69c92d65098d845795e1d92559720
ninetailsrabbit <danieldigitalspace@proton.me>
2024-08-13 09:58:21 +0100

    few helper methods on RoomMesh to change materials easily

--- components/map/room_creator/RoomMesh.cs
+++ components/map/room_creator/RoomMesh.cs
@@ -1,12 +1,15 @@
-﻿using Godot;
+﻿using Extensionator;
+using Godot;
+using System.Collections.Generic;
 
 namespace GodotExtensionatorStarter {
     [Tool]
     public partial class RoomMesh : MeshInstance3D {
-        #region Walls
         public ArrayMesh RoomMeshShape { get; set; } = null!;
         public Material FloorMaterial { get; set; } = default!;
         public Material CeilMaterial { get; set; } = default!;
+
+        #region Walls
         public Material FrontWallMaterial { get; set; } = default!;
         public Material BackWallMaterial { get; set; } = default!;
         public Material LeftWallMaterial { get; set; } = default!;
@@ -14,10 +17,10 @@ namespace GodotExtensionatorStarter {
         #endregion
 
         #region Door slots
-        public Material FrontWallDoorSlootMaterial { get; set; } = default!;
-        public Material BackWallDoorSlootMaterial { get; set; } = default!;
-        public Material RightWallDoorSlootMaterial { get; set; } = default!;
-        public Material LeftWallDoorSlootMaterial { get; set; } = default!;
+        public Material FrontWallDoorSlotMaterial { get; set; } = default!;
+        public Material BackWallDoorSlotMaterial { get; set; } = default!;
+        public Material RightWallDoorSlotMaterial { get; set; } = default!;
+        public Material LeftWallDoorSlotMaterial { get; set; } = default!;
         #endregion
 
         public override void _EnterTree() {
@@ -28,13 +31,68 @@ namespace GodotExtensionatorStarter {
             }
         }
 
+        public IEnumerable<Material> WallMaterials() {
+            List<Material> wallMaterials = [FrontWallMaterial, BackWallMaterial, RightWallMaterial, LeftWallMaterial];
+
+            return wallMaterials.RemoveNullables();
+        }
+
+        public IEnumerable<Material> DoorSlotsMaterials() {
+            List<Material> doorSlotMaterials = [FrontWallDoorSlotMaterial, BackWallDoorSlotMaterial, RightWallDoorSlotMaterial, LeftWallDoorSlotMaterial];
+
+            return doorSlotMaterials.RemoveNullables();
+        }
+
         public Material? GetSurfaceMaterial(string name) {
             int surfaceIndex = RoomMeshShape.SurfaceFindByName(name);
 
             return surfaceIndex == -1 ? null : RoomMeshShape.SurfaceGetMaterial(surfaceIndex);
         }
 
-        public void UpdateRoomMaterials() {
+        public RoomMesh ChangeMaterialOnFloor(Material newMaterial) {
+            FloorMaterial = newMaterial;
+
+            return this;
+        }
+
+        public void ChangeMaterialOnCeil(Material newMaterial) {
+            CeilMaterial = newMaterial;
+        }
+        public RoomMesh ChangeMaterialOnWalls(Material newMaterial) {
+            if (RightWallMaterial is not null)
+                RightWallMaterial = newMaterial;
+
+            if (LeftWallMaterial is not null)
+                LeftWallMaterial = newMaterial;
+
+            if (FrontWallMaterial is not null)
+                FrontWallMaterial = newMaterial;
+
+            if (BackWallMaterial is not null)
+                BackWallMaterial = newMaterial;
+
+            return this;
+
+        }
+
+        public RoomMesh ChangeMaterialOnDoorSlots(Material newMaterial) {
+            if (RightWallDoorSlotMaterial is not null)
+                RightWallDoorSlotMaterial = newMaterial;
+
+            if (LeftWallDoorSlotMaterial is not null)
+                LeftWallDoorSlotMaterial = newMaterial;
+
+            if (FrontWallDoorSlotMaterial is not null)
+                FrontWallDoorSlotMaterial = newMaterial;
+
+            if (BackWallDoorSlotMaterial is not null)
+                BackWallDoorSlotMaterial = newMaterial;
+
+            return this;
+
+        }
+
+        private void UpdateRoomMaterials() {
             RoomMeshShape ??= (ArrayMesh)Mesh;
 
             FloorMaterial = GetSurfaceMaterial("Floor");
@@ -44,10 +102,10 @@ namespace GodotExtensionatorStarter {
             LeftWallMaterial = GetSurfaceMaterial("LeftWall");
             RightWallMaterial = GetSurfaceMaterial("RightWall");
 
-            RightWallDoorSlootMaterial = GetSurfaceMaterial("RightWallDoorSlot");
-            LeftWallMaterial = GetSurfaceMaterial("LeftWallDoorSlot");
-            FrontWallMaterial = GetSurfaceMaterial("FrontWallDoorSlot");
-            BackWallMaterial = GetSurfaceMaterial("BackWallDoorSlot");
+            RightWallDoorSlotMaterial = GetSurfaceMaterial("RightWallDoorSlot");
+            LeftWallDoorSlotMaterial = GetSurfaceMaterial("LeftWallDoorSlot");
+            FrontWallDoorSlotMaterial = GetSurfaceMaterial("FrontWallDoorSlot");
+            BackWallDoorSlotMaterial = GetSurfaceMaterial("BackWallDoorSlot");
         }
 
     }
