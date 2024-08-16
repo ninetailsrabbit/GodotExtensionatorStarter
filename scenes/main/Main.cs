11ff75ddad94b313561d2cc6144f1a6ae6271287
ninetailsrabbit <danieldigitalspace@proton.me>
2024-08-16 12:26:54 +0100

    updated Main.cs and added two effects shaders related to images

--- scenes/main/Main.cs
+++ scenes/main/Main.cs
@@ -4,12 +4,27 @@ using GodotExtensionator;
 namespace GodotExtensionatorStarter {
     public partial class Main : Node {
 
-        [Export] public bool ShowPhotosensitiveContentWarning = true;
-
+        [Export(PropertyHint.File, "*.tscn")] public string NextScene = string.Empty;
+        public ContentWarningsDisplayer ContentWarningsDisplayer { get; set; } = default!;
         public SceneTransitioner SceneTransitioner { get; set; } = default!;
+        public override void _ExitTree() {
+            if (ContentWarningsDisplayer is not null)
+                ContentWarningsDisplayer.AllContentWarningsDisplayed -= OnContentWarningsDisplayFinished;
+        }
 
         public override void _EnterTree() {
             SceneTransitioner = this.GetAutoloadNode<SceneTransitioner>();
+        }
+        public override void _Ready() {
+            ContentWarningsDisplayer = this.FirstNodeOfClass<ContentWarningsDisplayer>();
+
+            if (ContentWarningsDisplayer is not null)
+                ContentWarningsDisplayer.AllContentWarningsDisplayed += OnContentWarningsDisplayFinished;
+        }
+
+
+        private void OnContentWarningsDisplayFinished() {
+            SceneTransitioner.TransitionToScene(NextScene);
         }
     }
 
